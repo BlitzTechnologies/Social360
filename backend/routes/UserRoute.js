ec8037a9210640a7526a0167d46df8e2b77389ca
brian <brianyuk@gmail.com>
2024-08-02 15:12:25 +0800

    update imports

--- backend/routes/UserRoute.js
+++ backend/routes/UserRoute.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const userRepository = require('../repositories/UserRepository');
 const { validateRegisterUser, hashPassword } = require('../modules/accounts/userHelper');
 const { MONGO_DB_COLLECTIONS } = require('../modules/constants');
-const User = require('../models/User');
 const { UserMapper } = require('../mappers/AccountMapper');
 
 
@@ -43,6 +42,26 @@ router.post('/', async (req, res) => {
     console.error("Error creating user:", error);
     return res.status(500).json({ message: 'Internal Server Error' });
   }
+});
+
+// route for on blur when filling in username
+router.post('/check/username', async (req, res) => {
+  let reqObj = req.body;
+  let userList = await userRepository.getAllUsers(MONGO_DB_COLLECTIONS.ACCOUNT_COLLECTION)
+  if (userList.some(user => user.username === reqObj.username)) {
+    return res.status(400).json({ message: "username already exists" })
+  }
+  return res.status(200).json({ message: "username is unique" })
+});
+
+// route for on blur when filling in email
+router.post('/check/email', async (req, res) => {
+  let reqObj = req.body;
+  let userList = await userRepository.getAllUsers(MONGO_DB_COLLECTIONS.ACCOUNT_COLLECTION)
+  if (userList.some(user => user.email === reqObj.email)) {
+    return res.status(400).json({ message: "email already exists" })
+  }
+  return res.status(200).json({ message: "email is unique" })
 });
 
 router.put('/:id', (req, res) => {
