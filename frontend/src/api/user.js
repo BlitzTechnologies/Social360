2eb00c84319406732b940fbba3a1c4f20440a090
brian <brianyuk@gmail.com>
2024-08-04 17:06:29 +0800

    check username and email on blur

--- frontend/src/api/user.js
+++ frontend/src/api/user.js
@@ -1,7 +1,13 @@
 import request from "./request";
 
-
 export const register = ({ fullName, email, username, password }) => {
-  return request.post("/users/", { fullName, email, username, password });
+    return request.post("/users/", { fullName, email, username, password });
 };
 
+export const checkEmail = ({ email }) => {
+    return request.post("/users/check/email", { email });
+};
+
+export const checkUsername = ({ username }) => {
+    return request.post("/users/check/username", { username });
+};
