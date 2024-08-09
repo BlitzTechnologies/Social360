f59d1a72e83bd5d798d4faf79f568bccde2f6e2f
brian <brianyuk@gmail.com>
2024-08-09 23:25:21 +0800

    base mediasoup and webrtc init

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,7 @@ const roomRoutes = require('./routes/RoomRoute.js'); // Import the route normall
 const cors = require("cors");
 const http = require('http');
 const { Server } = require('socket.io');
+const mediasoup = require('mediasoup'); // Import Mediasoup
 
 const app = express();
 
@@ -22,28 +23,6 @@ app.use(express.json());
 
 app.use('/auth', AuthRoutes);
 app.use('/users', userRoutes);
-
-// Create an HTTP server and integrate with Socket.IO
-const server = http.createServer(app);
-const IO = new Server(server, {
-  cors: {
-    origin: process.env.CLIENT_URL,
-    methods: ["GET", "POST"]
-  }
-});
-
-const peers = IO.of('/mediasoup');
-
-peers.on('connection', (socket) => {
-  console.log(socket.id);
-  socket.emit('connection-success', {
-    socketId: socket.id
-  })
-})
-
-// Export the io instance
-module.exports = IO;
-
 app.use('/rooms', roomRoutes);
 
 app.use((req, res, next) => {
@@ -58,6 +37,69 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.send('About Page');
 });
+
+// Create an HTTP server and integrate with Socket.IO
+const server = http.createServer(app);
+const IO = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST"]
+  }
+});
+
+const peers = IO.of('/mediasoup');
+
+let worker;
+let router;
+
+const createWorker = async () => {
+  worker = await mediasoup.createWorker({
+    rtcMinPort: 2000,
+    rtcMaxPort: 2020,
+  })
+  console.log(`worker pid ${worker.pid}`)
+
+  worker.on('died', error => {
+    // This implies something serious happened, so kill the application
+    console.error('mediasoup worker has died')
+    setTimeout(() => process.exit(1), 2000) // exit in 2 seconds
+  })
+
+  return worker
+}
+
+// We create a Worker as soon as our application starts
+worker = createWorker()
+
+const mediaCodecs = [
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    clockRate: 48000,
+    channels: 2,
+  },
+  {
+    kind: 'video',
+    mimeType: 'video/VP8',
+    clockRate: 90000,
+    parameters: {
+      'x-google-start-bitrate': 1000,
+    },
+  },
+]
+
+
+peers.on('connection', async (socket) => {
+  console.log(socket.id);
+  socket.emit('connection-success', {
+    socketId: socket.id
+  })
+  socket.on('disconnect', () => {
+    // do some cleanup
+    console.log('peer disconnected')
+  })
+  router = await worker.createRouter({ mediaCodecs, })
+})
 
 server.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
